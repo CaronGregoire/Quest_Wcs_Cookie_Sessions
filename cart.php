a3e65dd36926af3249b391720bc0eaccef20151f
CaronGregoire <gregoirecaron33@gmail.com>
2021-11-01 11:09:38 +0100

    file for quest

---
+++ cart.php
@@ -0,0 +1,22 @@
+<?php
+require 'inc/head.php';
+require 'inc/data/products.php';
+require 'connect.php';
+?>
+<section class="cookies container-fluid">
+    <div class="row">
+
+        <div class="products">
+            <?php if(isset($_SESSION['cookies']));?>
+                <ul>
+                    <?php $cookies = $_SESSION['cookies'];
+                    foreach ($cookies as $id => $count){
+                    $cookieName = $catalog[$id]['name'];?>
+                    <li><?= $count . ' - '. $cookieName ?> </li>
+                    <?php } ?>
+                </ul>
+
+        </div>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
